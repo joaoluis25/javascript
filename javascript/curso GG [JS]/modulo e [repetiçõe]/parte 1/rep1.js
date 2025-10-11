8669f73710d281a9b8b0ca67ea8e1ba4033fb135
joaoluis25 <180711519+joaoluis25@users.noreply.github.com>
2025-10-10 21:37:10 -0300

    repetições

---
+++ javascript/curso GG [JS]/modulo e [repetiçõe]/parte 1/rep1.js
@@ -0,0 +1,15 @@
+/*console.log('ola mundo')*/
+
+/*
+var c = 1
+while (c <=  6){
+    console.log(`passo ${c}`)
+    c++ //mesma coisa de c recebe c + 1
+}
+*/
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 9)
---
+++ javascript/curso GG [JS]/modulo e [repetiçõe]/parte 2/rep1.js
@@ -0,0 +1,13 @@
+ /*
+ var c = 1
+ while (c <= 10){
+    console.log(`Passo ${c}`)
+    c++
+ }
+*/
+
+//Usando for
+
+for (var c = 1; c <=10; c++ ){
+    console.log(`Passo ${c}`)
+}
