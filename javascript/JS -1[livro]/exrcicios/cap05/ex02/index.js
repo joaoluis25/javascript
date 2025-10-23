9c8d69a6846485755665a4feaefea8e3c6f4dd0d
joaoluis25 <180711519+joaoluis25@users.noreply.github.com>
2025-10-23 14:20:12 -0300

    cap 03 terminei [for]

--- javascript/JS -1[livro]/exrcicios/cap05/ex02/index.js
+++ javascript/JS -1[livro]/exrcicios/cap05/ex02/index.js
@@ -5,10 +5,19 @@ frm.addEventListener('submit', (e)=>{
     e.preventDefault()
     const numero = Number(frm.inNum.value)
     const decrescente = Number(frm.inDec.value)
+    /*
+    FORMA INICIAL DE SE RESOLVER POREM FICA SOBRANDO UM ',' AO FINAL DA LISTAGEM DE NUMEROS, OQUE GERA ESTRANHESA E DEIXA A RESPOSTA FEIA.
 
     let resposta = `Entre ${numero} e ${decrescente}:`
     for(let i = numero; i > 0; i = i - decrescente){
         resposta = resposta + i + ',' 
     }
+    */
+   // CORRIGINDO O PROBLEMA DA VIRGULA
+   let resposta = `Entre ${numero} e 1: ${numero}`
+   for (let i = numero - 1; i > 0; i--){
+    resposta = resposta + ',' + i
+   }
     resp.innerText = resposta
+
 })
