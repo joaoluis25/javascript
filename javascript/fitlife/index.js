481572c7c9cf312de8c18b9343525179fb51ebd2
joaoluis25 <180711519+joaoluis25@users.noreply.github.com>
2025-12-16 01:13:47 -0300

    header complete

---
+++ javascript/fitlife/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/*
+   ADD EVENT ON ELEMENT
+*/ 
+
+const addEventOnElem = function(elem, type, callback){
+    if(elem.length > 1){
+        for(let i = 0; i < elem.length; i++){
+            elem[i].addEventListener(type , callback)
+        }
+    } else {
+        elem.addEventListener(type, callback)
+    }
+}
+
+/*
+    NAVBAR TOGGLE
+*/ 
+
+    const navbar = document.querySelector('[data-navbar]')
+    const navTogglers = document.querySelectorAll('[data-nav-toggler]')
+    const navLinks = document.querySelectorAll("[data-nav-link]")
+
+    const toggleNavbar = function () {navbar.classList.toggle('active')} //toggle ative o active 
+
+    addEventOnElem(navTogglers, 'click', toggleNavbar)
+
+    const closeNavbar = function () {navbar.classList.remove('active')}//remove desabilita o active
+
+    addEventOnElem(navLinks, 'click', closeNavbar)
